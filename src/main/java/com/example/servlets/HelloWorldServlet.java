63a29e57d9bf90e289d5208a09c4e37f7ef1ff6d
Verkhoturkin <verhoturkin@gmail.com>
2020-05-21 09:21:25 +0500

    Revert "step 58"

---
+++ src/main/java/com/example/servlets/HelloWorldServlet.java
@@ -0,0 +1,26 @@
+package com.example.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet("/hws")
+public class HelloWorldServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        out.println("<h2>Hello World!</h2>");
+        out.println("<hr>");
+        out.println("Tome on the server is:" + new Date());
+        out.println("</body></html>");
+    }
+}
